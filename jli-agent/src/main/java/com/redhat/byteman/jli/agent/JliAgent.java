acad34f9ea4bd5d4e7d87decc54d38c688d4d8f9
akashche <akashche@redhat.com>
2015-04-30 20:42:46 +0100

    j.l.instrument example

---
+++ jli-agent/src/main/java/com/redhat/byteman/jli/agent/JliAgent.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2015 Red Hat, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+package com.redhat.byteman.jli.agent;
+
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.lang.instrument.Instrumentation;
+import java.security.ProtectionDomain;
+
+/**
+ * java.lang.instrument hello world agent example
+ */
+public class JliAgent {
+
+    public static void premain(String agentArgs, Instrumentation inst) {
+        inst.addTransformer(new JliTransformer());
+        System.out.println("Hello from premain!");
+    }
+
+}
